15f617f6828d2e06f04f22cc550a5e018f778277
Zhixin Zeng <68415715+Crimson725@users.noreply.github.com>
2025-03-20 16:50:54 -0700

    update

--- src/constants/constants.ts
+++
@@ -1,5 +0,0 @@
-const SERVER_BASE_URL =
-  import.meta.env.MODE === "production"
-    ? import.meta.env.VITE_BACKEND_URL
-    : "http://localhost:3000";
-export { SERVER_BASE_URL };
--- src/types/user.ts
+++
@@ -1,8 +0,0 @@
-export interface User {
-  _id: string;
-  name: string;
-  email: string;
-  photo: string;
-  role: "user" | "guide" | "lead-guide" | "admin";
-  active?: boolean;
-} 
