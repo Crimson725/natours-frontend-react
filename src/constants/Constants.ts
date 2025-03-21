e54a8a0be65159604437951c22930e713b07ac45
Zhixin Zeng <68415715+Crimson725@users.noreply.github.com>
2025-03-20 23:02:21 -0700

    Update Constants.ts

--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -1,5 +1,2 @@
-const SERVER_BASE_URL =
-  import.meta.env.MODE === "production"
-    ? import.meta.env.VITE_BACKEND_URL
-    : "https://natours-2p2d.onrender.com";
+const SERVER_BASE_URL ="https://natours-2p2d.onrender.com";
 export { SERVER_BASE_URL };
